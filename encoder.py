Fix average loss twice 1. Add decode locations and labels by batch 2. Use gather(reduce size) instead with element-wise multiplication 3. Visualize the boundingbox when validate

encoder.py
@@ -104,8 +104,13 @@ class BoxEncoder:
         cls_trues = tf.cast(cls_trues, tf.float32)
         return loc_trues, cls_trues
 
-    def decode(self, loc_preds, cls_preds, input_size,
-               cls_thred=conf.cls_thred, nms_thred=conf.nms_thred, return_score=False):
+    def decode(self, loc_preds, cls_preds,
+               input_size=conf.input_size,
+               cls_thred=conf.cls_thred,
+               max_output_size=conf.max_output_size,
+               nms_thred=conf.nms_thred,
+               return_score=False,
+               tf_box_order=True):
         """Decode outputs back to bouding box locations and class labels.
 
         Args:
@@ -113,11 +118,16 @@ class BoxEncoder:
             cls_preds: (tensor) predicted class labels, sized [#anchors, #classes].
             input_size: (int/tuple) model input size of (w, h), should be the same.
             cls_thred: class score threshold
+            max_output_size: max output nums after nms
             nms_thred: non-maximum suppression threshold
             return_score: (bool) indicate whether to return score value.
+            tf_box_order: (bool) True: [ymin, xmin, ymax, xmax]
+                                False: [xmin, ymin, xmax, ymax]
         Returns:
-            boxes: (tensor) decode box locations, sized [#obj, 4]. [xmin, ymin, xmax, ymax]
+            boxes: (tensor) decode box locations, sized [#obj, 4].
+                            order determined by param: tf_box_order
             labels: (tensor) class labels for each box, sized [#obj, ].
+            NOTE: #obj == min(#detected_objs, #max_output_size)
         """
         assert len(loc_preds.get_shape().as_list()) == 2, 'Ensure the location input shape to be [#anchors, 4]'
         assert len(cls_preds.get_shape().as_list()) == 2, 'Ensure the class input shape to be [#anchors, #classes]'
@@ -137,10 +147,17 @@ class BoxEncoder:
 
         ids = tf.cast(score > cls_thred, tf.int32)
         ids = tf.squeeze(tf.where(tf.not_equal(ids, 0)))
-        # ids.nonzero().squeeze()  # [#obj, ]
         if not ids.get_shape().as_list():  # Fail to detect, choose the max score
             ids = tf.argmax(score)
-        keep = box_nms(tf.gather(boxes, ids), tf.gather(score, ids), threshold=nms_thred)
+        if tf_box_order:  # [ymin, xmin, ymax, xmax]
+            boxes = tf.transpose(tf.gather(tf.transpose(boxes), [1, 0, 3, 2]))
+            keep = tf.image.non_max_suppression(tf.gather(boxes, ids),
+                                                tf.gather(score, ids),
+                                                max_output_size=max_output_size,
+                                                iou_threshold=nms_thred)
+        else:  # [xmin, ymin, xmax, ymax]
+            keep = box_nms(tf.gather(boxes, ids), tf.gather(score, ids),
+                           threshold=nms_thred)
 
         def _index(t, index):
             """Gather tensor successively
@@ -155,6 +172,41 @@ class BoxEncoder:
         if return_score:
             return _index(boxes, [ids, keep]), _index(labels, [ids, keep]), _index(score, [ids, keep])
         return _index(boxes, [ids, keep]), _index(labels, [ids, keep])
+
+    def decode_batch(self,
+                     batch_loc_preds,
+                     batch_cls_preds,
+                     input_size=conf.input_size,
+                     tf_box_order=True):
+        """Choose the most confident one from multiple (if any) predictions per image.
+        Make sure each image only has one output (loc + cls)
+
+        Args:
+            batch_loc_preds: (tensor) predicted locations, sized [batch, #anchors, 4].
+            batch_cls_preds: (tensor) predicted class labels, sized [batch, #anchors, #classes].
+            input_size: (int/tuple) model input size of (w, h), should be the same.
+            tf_box_order: (bool) True: [ymin, xmin, ymax, xmax]
+                                False: [xmin, ymin, xmax, ymax]
+        Returns:
+            batch_loc: (tensor)  decode batch box locations, sized [batch, 4]. [y_min, x_min, y_max, x_max]
+            batch_cls: (tensor) class label for each box, sized [batch, ]
+            batch_scores: (tensor) score for each box, sized [batch, ]
+
+        """
+        batch_loc, batch_cls, batch_scores = [], [], []
+        for i, (loc_preds, cls_preds) in enumerate(zip(batch_loc_preds, batch_cls_preds)):
+            loc, cls, scores = self.decode(loc_preds, cls_preds,
+                                           input_size,
+                                           max_output_size=10,
+                                           return_score=True,
+                                           tf_box_order=tf_box_order)
+            if scores.shape[0] == 0:
+                return [None] * 3
+            max_score_id = tf.argmax(scores)
+            batch_loc.append(tf.gather(loc, max_score_id).numpy() / input_size[0])
+            for item in ['cls', 'scores']:
+                eval('batch_' + item).append(tf.gather(eval(item), max_score_id).numpy())
+        return [tf.convert_to_tensor(item, dtype=tf.float32) for item in [batch_loc, batch_cls, batch_scores]]
 
 
 def test():
@@ -171,9 +223,10 @@ def test():
 
             with tf.device("cpu:0"):
                 # Decode one by one in a batch
-                loc_preds, cls_preds = box_encoder.decode(loc_preds.cpu()[0], cls_preds.cpu()[0], input_size)
+                loc_preds, cls_preds, score = box_encoder.decode_batch(loc_preds.cpu(), cls_preds.cpu(), input_size)
                 print('loc_preds {} shape: {}'.format(loc_preds, loc_preds.shape))
                 print('cls_preds {} shape: {}'.format(cls_preds, cls_preds.shape))
+                print('score {} shape: {}'.format(score, score.shape))
             break
 
 

loss.py
@@ -66,35 +66,47 @@ def loss_fn(loc_preds, loc_trues, cls_preds, cls_trues, num_classes=12):
 
     # 1. loc_loss: tf.losses.huber_loss
     # ==================================================================
-    pos = tf.cast(tf.expand_dims(cls_trues > 0, -1), tf.float32)  # [N, #anchors]
-    masked_loc_preds = loc_preds * pos  # [#valid_pos, 4]
-    masked_loc_trues = loc_trues * pos  # [#valid_pos, 4]
+    # TODO: cannot use boolean_mask/slice between GPU and CPU
+    mask_index = tf.where(cls_trues > 0)
+    masked_loc_preds = tf.gather_nd(loc_preds, mask_index)  # [#valid_pos, 4]
+    masked_loc_trues = tf.gather_nd(loc_trues, mask_index)  # [#valid_pos, 4]
+
     loc_loss = tf.losses.huber_loss(masked_loc_preds, masked_loc_trues)
     # ==================================================================
 
     # 2. cls_loss = FocalLoss(loc_preds, loc_trues)
     # ==================================================================
-    pos_neg_2d = tf.cast(cls_trues > -1, tf.float32)
-    pos_neg_3d = tf.expand_dims(pos_neg_2d, -1)  # exclude ignored anchors
-    masked_cls_preds = tf.reshape(cls_preds * pos_neg_3d, [-1, num_classes])  # [#valid_anchors, #cls]
-    masked_cls_trues = tf.reshape(cls_trues * pos_neg_2d, [-1])  # [#valid_anchors]
+    mask_index = tf.where(cls_trues > -1)
+    masked_cls_preds = tf.reshape(tf.gather_nd(cls_preds, mask_index), [-1, num_classes])  # [#valid_anchors, #cls]
+    masked_cls_trues = tf.reshape(tf.gather_nd(cls_trues, mask_index), [-1])  # [#valid_anchors]
+
     # cls_loss = focal_loss_alt(masked_cls_preds, masked_cls_trues, num_classes)
     cls_loss = focal_loss(masked_cls_preds, masked_cls_trues, num_classes)
     # ==================================================================
-    num_pos = tf.reduce_sum(tf.cast(pos, tf.float32))
-    loss = (loc_loss + cls_loss) / num_pos
 
-    return loss
+    return loc_loss, cls_loss
 
 
 def test():
+    # with tf.device("/gpu:0"):
+    # [batch_size, #anchors]s
+    # loc_preds = tf.random_uniform([3, 10, 4])
+    # loc_trues = tf.random_uniform([3, 10, 4])
+    # cls_preds = tf.random_uniform([3, 10, 12])
+    # cls_trues = tf.random_uniform([3, 10])
+
+    from inputs import dataset_generator
+    from retinanet import RetinaNet
+
+    dataset = dataset_generator('train', (448, 448), 1, 16, 100)
+    model = RetinaNet()
+
     with tf.device("/gpu:0"):
-        # [batch_size, #anchors]s
-        loc_preds = tf.random_uniform([3, 10, 4])
-        loc_trues = tf.random_uniform([3, 10, 4])
-        cls_preds = tf.random_uniform([3, 10, 12])
-        cls_trues = tf.random_uniform([3, 10])
-        print("Loss at step 0: %f" % (loss_fn(loc_preds, loc_trues, cls_preds, cls_trues).numpy()))
+        for i, (image, loc_trues, cls_trues) in enumerate(tfe.Iterator(dataset)):
+            loc_preds, cls_preds = model(image, training=True)
+            loc_loss, cls_loss = loss_fn(loc_preds, loc_trues, cls_preds, cls_trues)
+            print("Step 0: Location loss: {:.5f}  |  Class loss: {:.5f}".format(loc_loss.numpy(), cls_loss.numpy()))
+            break
 
 
 if __name__ == '__main__':

retinanet.py
@@ -76,8 +76,8 @@ class RetinaNet(tfe.Network):
 
 def test():
     with tf.device("gpu:0"):
-        image = tf.random_uniform([3, 1, 448, 448])
         model = RetinaNet()
+        image = tf.random_uniform([3, 1, 448, 448])
         loc_preds, cls_preds = model(image)
         print('loc_preds shape: {}'.format(loc_preds.shape))
         print('cls_preds shape: {}'.format(cls_preds.shape))

train.py
@@ -10,6 +10,7 @@ import tensorflow.contrib.eager as tfe
 
 from loss import loss_fn
 from inputs import dataset_generator
+from encoder import BoxEncoder
 from retinanet import RetinaNet
 from configuration import conf
 
@@ -27,32 +28,44 @@ def train_one_epoch(model, optimizer, dataset, log_interval=None):
 
     def model_loss(images, loc_trues, cls_trues):
         loc_preds, cls_preds = model(images, training=True)
-        loss_value = loss_fn(loc_preds, loc_trues, cls_preds, cls_trues, num_classes=conf.num_class)
-        tf.contrib.summary.scalar('loss', loss_value)
-        return loss_value
+        loc_loss, cls_loss = loss_fn(loc_preds, loc_trues, cls_preds, cls_trues, num_classes=conf.num_class)
+        total_loss = loc_loss + cls_loss
+        tf.contrib.summary.scalar('loc_loss', loc_loss)
+        tf.contrib.summary.scalar('cls_loss', cls_loss)
+        tf.contrib.summary.scalar('total_loss', total_loss)
+        return total_loss
 
     for batch, (images, loc_trues, cls_trues) in enumerate(tfe.Iterator(dataset)):
         with tf.contrib.summary.record_summaries_every_n_global_steps(10):
             gs = tf.train.get_global_step()
-            # Visualize the input images
-            tf.contrib.summary.image('inputs', tf.transpose(images, [0, 2, 3, 1]), max_images=2, global_step=gs)
             # Optimize the loss
             batch_model_loss = functools.partial(model_loss, images, loc_trues, cls_trues)
             optimizer.minimize(batch_model_loss, global_step=gs)
+            # Visualize the input images
+            tf.contrib.summary.image('inputs', tf.transpose(images, [0, 2, 3, 1]), max_images=2, global_step=gs)
             if log_interval and batch % log_interval == 0:
-                print('==> ==> ==> Batch #{}\tLoss: {:.6f}'.format(batch, batch_model_loss().numpy()))
+                print('[TRAINING] Batch #{}\ttotal loss: {:.6f}'.format(batch, batch_model_loss().numpy()))
 
 
-def validate(model, dataset):
+def validate(model, dataset, visualize=True):
     """Perform an evaluation of `model` on the examples from `dataset`."""
 
     total_loss = 0.
     for batch, (images, loc_trues, cls_trues) in enumerate(tfe.Iterator(dataset)):
         loc_preds, cls_preds = model(images)
-        loss = loss_fn(loc_preds, loc_trues, cls_preds, cls_trues, num_classes=conf.num_class)
-        total_loss += loss
-        if batch % 10 == 0:
-            print('==> ==> ==> Batch #{}\t Test loss: {:.6f}'.format(batch, loss.numpy()))
+        if visualize:
+            with tf.device("cpu:0"):
+                box = BoxEncoder()
+                boxes, labels, scores = box.decode_batch(loc_preds.cpu(), cls_preds.cpu())
+                if boxes is not None:  # do detect something
+                    image = tf.image.draw_bounding_boxes(tf.transpose(images.cpu(), [0, 2, 3, 1]),
+                                                         tf.expand_dims(boxes, 1))
+                    tf.contrib.summary.image('validate', image, global_step=tf.train.get_global_step())
+        loc_loss, cls_loss = loss_fn(loc_preds, loc_trues, cls_preds, cls_trues, num_classes=conf.num_class)
+        total_loss += loc_loss + cls_loss
+        if batch % conf.log_interval == 0:
+            print('[EVALUATION] Batch #{}\tloc_loss: {:.6f} | cls_loss: {:.6f} | total_loss: {:.6f}'.format(
+                    batch, loc_loss.numpy(), cls_loss.numpy(), (loc_loss + cls_loss).numpy()))
 
     avg_loss = (total_loss / batch).numpy()
     print('Test set: Average loss: {:.6f}\n'.format(avg_loss))
@@ -63,15 +76,14 @@ def validate(model, dataset):
 def main(_):
     assert tfe.num_gpus() > 0, 'Make sure the GPU device exists'
     device_name = '/gpu:{}'.format(args.cuda_device)
-    print('==> ==> ==> Using device {}'.format(device_name))
+    print('\n==> ==> ==> Using device {}'.format(device_name))
 
     # Load the dataset
     train_ds, val_ds = [dataset_generator(mode,
                                           conf.input_size,
                                           num_epochs=1,
                                           batch_size=conf.batch_size,
-                                          buffer_size=100)
-                        # buffer_size=conf.buffer_size)
+                                          buffer_size=100)  # TODO edit this when in real training
                         for mode in ['train', 'val']]
 
     # Create the model and optimizer
@@ -88,19 +100,21 @@ def main(_):
     checkpoint_prefix = os.path.join(conf.checkpoint_dir, 'ckpt')
 
     with tf.device(device_name):
+        print('==> ==> ==> Start training from global step {}...\n'.format(
+                tf.train.get_or_create_global_step().numpy()))
         for epoch in range(conf.num_epochs):
             with tfe.restore_variables_on_create(tf.train.latest_checkpoint(conf.checkpoint_dir)):
-                global_step = tf.train.get_or_create_global_step()
+                gs = tf.train.get_or_create_global_step()
                 start = time.time()
                 with train_summary_writer.as_default():
                     train_one_epoch(model, optimizer, train_ds, conf.log_interval)
                 end = time.time()
                 print('==> ==> ==> Train time for epoch #{} (global step {}): {:.3f}s\n'.format(
-                        epoch, global_step.numpy(), end - start))
+                        epoch, gs.numpy(), end - start))
                 with val_summary_writer.as_default():
                     validate(model, val_ds)
-                all_variables = (model.variables + optimizer.variables() + [global_step])
-                tfe.Saver(all_variables).save(checkpoint_prefix, global_step=global_step)
+                all_variables = (model.variables + optimizer.variables() + [gs])
+                tfe.Saver(all_variables).save(checkpoint_prefix, global_step=gs)
 
 
 if __name__ == '__main__':